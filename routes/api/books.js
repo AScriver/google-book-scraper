b9f38b44dfc8325a104325aa207cf7096a771aae
ascriver <ascriver@live.com>
2018-12-07 08:29:41 -0700

    added some comments

--- routes/api/books.js
+++ routes/api/books.js
@@ -8,9 +8,14 @@ router.route("/")
 
 // Matches with "/api/books/:id"
 router
-  .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
-  .delete(booksController.remove);
-
+  .route("/saved")
+  .get(booksController.findSaved)
+  
 module.exports = router;
+
+// /api/books (get)
+// /api/books (post)
+// api/books/:id (delete)
+// * (get - load singlt HTML page) => Define this after all other routes
+
+// https://www.googleapis.com/books/v1/volumes/mKfDFa8r3pYC
